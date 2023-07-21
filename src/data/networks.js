86fa2467bdd0da196ea89a1440f6118ee4322d06
Toastiixd <mika@chainlab.solutions>
2023-07-21 23:43:30 +0200

    added supported networks

--- src/data/networks.js
+++ src/data/networks.js
@@ -3,6 +3,51 @@ const supportedNetworks = [
         name: 'Ethereum',
         chainId: 1
     }
+    ,
+    {
+        name: 'Polygon',
+        chainId: '137'
+    }
+    ,
+    {
+        name: 'BNB',
+        chainId: '56'
+    }
+    ,
+    {
+        name: 'Aurora',
+        chainId: '1313161554 '
+    }
+    ,
+    {
+        name: 'Gnosis',
+        chainId: '100'
+    }
+    ,
+    {
+        name: 'Mantle',
+        chainId: '5000'
+    }
+    ,
+    {
+        name: 'ZetaChain',
+        chainId: '7000'
+    }
+    ,
+    {
+        name: 'Celo',
+        chainId: '42220'
+    }
+    ,
+    {
+        name: 'OP',
+        chainId: '10'
+    }
+    ,
+    {
+        name: 'Cronos',
+        chainId: '25'
+    }
 ]
 
 export default supportedNetworks
